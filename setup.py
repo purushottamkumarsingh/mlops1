04215a5b65a1abec8d1af73ff164c4bdb877c850
purushottamkumarsingh <ravindrasingh813108@gmail.com>
2025-08-29 15:52:03 +0530

    setup.py file is added

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup 
+from typing import List  
+HYPEN_DOT ='-e .'
+
+def get_requirements(file_path:str) -> List[str] :
+  requirements = [] 
+  with open(file_path) as file_obj:
+    requirements = file_obj.readlines()
+    requirements = [req.replace("\n" ,"") for req in requirements]
+    if HYPEN_DOT in requirements:
+      requirements.remove(HYPEN_DOT)
+  return requirements
+
+
+setup(
+name ='mlproject',
+version='0.0.1',
+author ='purushottam kumar singh',
+author_email='ravindrasingh813108@gmail.com',
+packages =find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
